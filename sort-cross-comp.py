64e2bac2106f9d86b91c7a187d0ab2db8b828780
Jose <jgh9094@gmail.com>
2022-11-07 00:00:20 -0600

    added comments

--- sort-cross-comp.py
+++ sort-cross-comp.py
@@ -14,42 +14,59 @@ import numpy as np
 import pandas as pd
 import time
 
-# loop through differnt files that exist
+# selection sort implementation
 def SelectionSort(nums):
+    # start tracking time
     tic = time.perf_counter()
+
+    # iterate through all list except the last element
     for low in range(len(nums)-1):
         mp = low
-        for i in range(low + 1, len(nums)):
+        # sort through current unsorted list
+        for i in range(low+1, len(nums)):
+            # compare and swap if smaller element is found
             if nums[i] < nums[mp]:
                 mp = i
-            nums[low],nums[mp] = nums[mp],nums[low]
+        # swap start of unsorted list element with the the min element found in the same portion
+        (nums[low],nums[mp]) = (nums[mp],nums[low])
 
     # return time difference
-    return time.perf_counter() - tic
+    diff = time.perf_counter() - tic
+    # check to see if sorted
+    print('sorted:', all(nums[i] <= nums[i+1] for i in range(len(nums) - 1)))
+    return diff
 
-# selection sort with one split
-def SelectionSplit(l1, l2):
+# buddy selection sort: split list -> selection sort on both -> merge sorted lists
+def SelectionSplit(nums,size):
 
     # start time
-    final = []
-    start1,start2 = 0,0
     tic = time.perf_counter()
+    # list when we merge two lists back together
+    final = []
+    # iterators for each list half
+    start1,start2 = 0,0
+    # split lists
+    l1,l2 = nums[:size//2],nums[size//2:]
 
+    # perform selection sort on first half
     for low in range(len(l1)-1):
         mp = low
         for i in range(low + 1, len(l1)):
             if l1[i] < l1[mp]:
                 mp = i
-        l1[low],l1[mp] = l1[mp],l1[low]
+        (l1[low],l1[mp]) = (l1[mp],l1[low])
 
+    # perform selection sort on second half
     for low in range(len(l2)-1):
         mp = low
         for i in range(low + 1, len(l2)):
             if l2[i] < l2[mp]:
                 mp = i
-        l2[low],l2[mp] = l2[mp],l2[low]
+        (l2[low],l2[mp]) = (l2[mp],l2[low])
 
-    while start1 != len(l1) and start2 != len(l2):
+    # merge both sorted lists back together
+    # while we have elements in either to merge
+    while start1 < len(l1) and start2 < len(l2):
         # check if l1 is smaller
         if l1[start1] < l2[start2]:
             final.append(l1[start1])
@@ -58,25 +75,34 @@ def SelectionSplit(l1, l2):
             final.append(l2[start2])
             start2+= 1
 
-    # get remaining vals in l1
+    # add remaining elements from first half split
     while start1 < len(l1):
         final.append(l1[start1])
         start1+= 1
 
-    # get remaining vals in l2
+    # add remaining elements from second half split
     while start2 < len(l2):
         final.append(l2[start2])
         start2+= 1
 
-    return time.perf_counter() - tic
+    # record time difference
+    diff = time.perf_counter() - tic
+    # quick check to make sure actually sorted
+    print('sorted:', all(final[i] <= final[i+1] for i in range(len(final) - 1)))
+
+    return diff
 
 # merge sort
 def MergeSort(nums):
-    # recursive step
+    # recursive step if we have more than one element
+    # stoping case is when we have only 1 element
     if 1 < len(nums):
+        # split the lists
         a,b = nums[:len(nums)//2],nums[len(nums)//2:]
+        # recursion
         MergeSort(a)
         MergeSort(b)
+        # merge
         Merge(a,b,nums)
 
 # merger
@@ -84,7 +110,9 @@ def Merge(l1,l2,l3):
     # starting positions
     start1,start2,start3 = 0,0,0
 
-    while start1 != len(l1) and start2 != len(l2):
+    # merge both sorted lists back together
+    # while we have elements in either to merge
+    while start1 < len(l1) and start2 < len(l2):
         # check if l1 is smaller
         if l1[start1] < l2[start2]:
             l3[start3] = l1[start1]
@@ -94,94 +122,135 @@ def Merge(l1,l2,l3):
             start2+= 1
         start3+= 1
 
-    # get remaining vals in l1
+    # add remaining elements from first half split
     while start1 < len(l1):
         l3[start3] = l1[start1]
         start1+= 1
         start3+= 1
 
-    # get remaining vals in l2
+    # add remaining elements from second half split
     while start2 < len(l2):
         l3[start3] = l2[start2]
         start2+= 1
         start3+= 1
 
-def main():
-    # set seed so we can get consistent results
-    np.random.seed(94)
-    # list of list sizes we care about
-    # sizes = [5,10,15,20] # small
-    sizes = [100,200,300,400] # big
-    times,N,sorter = [],[],[]
-    upper = 100000000
-    reps = 10
+def BigOrSmall(bos):
+    # data that will depend on big or small run
+    sizes = []
+    file_name = ''
+    # max random number possible
+    upper = 1000000
+    # number of replicates
+    reps = 20
 
-    # standard selection sort reps
+    # set seed so we can get consistent results
+    # you can change this to get different random lists
+    if bos: # big
+        # random seed for reproducability
+        np.random.seed(90)
+        # different sizes we are assessing
+        sizes = [100,200,400]
+        # name of image we are saving
+        file_name = 'big-sort-comp.png'
+    else: # small
+        # random seed for reproducability
+        np.random.seed(94)
+        # different sizes we are assessing
+        sizes = [5,10,15,20]
+        # name of image we are saving
+        file_name = 'small-sort-comp.png'
+
+
+    # data tracking
+    times,N,sorter = [],[],[]
+    # max random number possible
+    upper = 1000000
+    # number of replicates
+    reps = 20
+
+    # standard selection sort loop
     for i in range(len(sizes)):
+        # what size are we on?
         size = sizes[i]
         print('size:',size)
+
+        # get multiple data replicates
         for j in range(reps):
             print('rep:',j)
-            # get random list of numbers
-            nums = np.random.randint(0,upper, size)
 
-            # sort
-            t = SelectionSort(nums)
+            # get random list of numbers of a designated size and range [0,upper]
+            ran = np.random.randint(0,upper, size)
+            # sort & time
+            t = SelectionSort(ran)
 
-            #record data
+            #record data for plots
             times.append(t)
             N.append(size)
-            sorter.append('Selection')
+            sorter.append('Selection Sort')
         print()
 
-    # single split selection sort reps
+    # buddy selection sort loop
     for i in range(len(sizes)):
+        # what size are we on?
         size = sizes[i]
-        print(size)
+        print('size:',size)
+
+        # get multiple data replicates
         for j in range(reps):
             print('rep:',j)
-            # get random list of numbers
+
+            # get random list of numbers of a designated size and range [0,upper]
             nums = np.random.randint(0,upper, size)
-            l1,l2 = nums[:size//2],nums[size//2:]
+            # sort & time
+            t = SelectionSplit(nums,size)
 
-            # sort
-            t = SelectionSplit(l1,l2)
-
-            #record data
+            #record data for plots
             times.append(t)
             N.append(size)
-            sorter.append('Half')
+            sorter.append('Buddy Selection Sort')
+
         print()
 
-    # single split selection sort reps
+    # merge sort
     for i in range(len(sizes)):
+        # what size are we on?
         size = sizes[i]
-        print(size)
+        print('size:',size)
+
+        # get multiple data replicates
         for j in range(reps):
             print('rep:',j)
+
             # get random list of numbers
             nums = np.random.randint(0,upper, size)
 
-            # sort
+            # sort & time
             tic = time.perf_counter()
             MergeSort(nums)
             toc = time.perf_counter()
 
+            # make sure the list is sorted
+            print('sorted:', all(nums[i] <= nums[i+1] for i in range(len(nums) - 1)))
+
             #record data
             times.append(toc - tic)
             N.append(size)
-            sorter.append('Merge')
+            sorter.append('Merge Sort')
         print()
 
-    # plot data in a violin plot
+    # create pandas data frame, as seaborn expects that
     df = pd.DataFrame({'List size': N, 'Seconds': times, 'Sort': sorter})
-    sea.lineplot(data=df, x='List size', y="Seconds", hue='Sort')
-    plt.xticks(sizes)
+    # plot into a violin plot
+    sea.violinplot(data=df, x='List size', y="Seconds", hue='Sort')
     plt.legend(loc='upper left')
-    
-    # plt.savefig('small-sort-comp.png',dpi=500,bbox_inches='tight')
-    plt.savefig('big-sort-comp.png',dpi=1000,bbox_inches='tight')
-    # plt.show()
+    #save
+    plt.savefig(file_name,dpi=1000,bbox_inches='tight')
+    # plt.show() # uncomment if you want to see plot
+    plt.clf()
+
+def main():
+    BigOrSmall(False)
+    BigOrSmall(True)
 
 if __name__ == "__main__":
     main()
